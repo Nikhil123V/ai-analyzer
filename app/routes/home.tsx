360c2d14cc56b51cce90a62325a938690605e540
Nikhil123V <128300239+Nikhil123V@users.noreply.github.com>
2025-07-26 08:02:18 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -44,11 +44,13 @@ export default function Home() {
 
     <section className="main-section">
       <div className="page-heading py-16">
-        <h1>Track Your Applications & Resume Ratings</h1>
+        
+      <h3>Optimize Your Path to Success</h3>
+      <h1>Elevate Your Resume & Track Progress</h1>   
         {!loadingResumes && resumes?.length === 0 ? (
             <h2>No resumes found. Upload your first resume to get feedback.</h2>
         ): (
-          <h2>Review your submissions and check AI-powered feedback.</h2>
+          <h2>Get AI-Driven Advice for Better Outcomes</h2>
         )}
       </div>
       {loadingResumes && (
